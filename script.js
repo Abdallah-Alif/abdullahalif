714b1b5c02acbbf6f5278f74ac22e6b13e2d60c1
Abdullah Alif <ifabdullah69@gmail.com>
2026-01-03 21:56:56 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-// Render complex math on load
-window.onload = () => {
-    katex.render("i\\hbar\\frac{\\partial}{\\partial t}\\Psi(x,t) = \\hat{H}\\Psi(x,t)", 
-    document.getElementById("schrodinger"));
-};
 
-// Console Easter Egg for Recruiters
-console.log("%c ABDULLAH ALIF | ADMISSIONS 2027 ", "background: #000; color: #00f2ff; font-size: 20px;");
-console.log("Candidate from: Dhaka College (HSC) & BPATC School (SSC)");
